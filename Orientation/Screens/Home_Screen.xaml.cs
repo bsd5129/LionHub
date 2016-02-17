546a02c073cfafa9ffbad329b819f4cc382d404f
draekoon <bsd5129@psu.edu>
2016-02-17 12:52:08 -0500

    Added HomeListItem Modified Home_Screen Added Missing Icons

--- Orientation/Screens/Home_Screen.xaml.cs
+++ Orientation/Screens/Home_Screen.xaml.cs
@@ -7,9 +7,82 @@ namespace Orientation
 {
 	public partial class Home_Screen : ContentPage
 	{
+		
 		public Home_Screen ()
 		{
 			InitializeComponent ();
+
+			NavigationPage.SetHasNavigationBar (this, false);
+
+			grid.Children.Add (new HomeListItem (this, "Services", "services.png"), 0, 0);
+			grid.Children.Add (new HomeListItem (this, "Favorites", "favorite.png"), 0, 1);
+			grid.Children.Add (new HomeListItem (this, "Rooms", "rooms.png"), 0, 2);
+			grid.Children.Add (new HomeListItem (this, "Scavenger Hunt", "scavengerHunt.png"), 0, 3);
+			grid.Children.Add (new HomeListItem (this, "Events", "events.png"), 0, 4);
+			grid.Children.Add (new HomeListItem (this, "Where Am I?", "whereAmI.png"), 0, 5);
+			grid.Children.Add (new HomeListItem (this, "Settings", "settings.png"), 0, 6);
+		}
+
+		public void pressHomeListItem(string name)
+		{
+			switch (name) {
+			case "Services":
+				pressServicesHomeListItem ();
+				break;
+			case "Favorites":
+				pressFavoritesHomeListItem ();
+				break;
+			case "Rooms":
+				pressRoomHomeListItem ();
+				break;
+			case "Scavenger Hunt":
+				pressScavengerHuntHomeListItem ();
+				break;
+			case "Events":
+				pressEventsHomeListItem ();
+				break;
+			case "Where Am I?":
+				pressWhereAmIHomeListItem ();
+				break;
+			case "Settings":
+				pressSettingsHomeListItem ();
+				break;
+			}
+		}
+
+		public void pressServicesHomeListItem()
+		{
+			Navigation.PushAsync (new Service_Search_Screen());
+		}
+
+		public void pressFavoritesHomeListItem()
+		{
+			Navigation.PushAsync (new Favorites_Screen());
+		}
+
+		public void pressRoomHomeListItem()
+		{
+			
+		}
+
+		public void pressScavengerHuntHomeListItem()
+		{
+			
+		}
+
+		public void pressEventsHomeListItem()
+		{
+			
+		}
+
+		public void pressWhereAmIHomeListItem()
+		{
+			Navigation.PushAsync (new Where_Am_I_Screen());
+		}
+
+		public void pressSettingsHomeListItem()
+		{
+
 		}
 	}
 }
---
+++ Orientation/Views/HomeListItem.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Orientation
+{
+	public partial class HomeListItem : ContentView
+	{
+
+		public HomeListItem (Home_Screen home, string name, string imageFile)
+		{
+			InitializeComponent ();
+			buttonText.Text = name;
+			image.Source = imageFile;
+
+			TapGestureRecognizer tap = new TapGestureRecognizer { NumberOfTapsRequired = 1 };
+
+			tap.Tapped += (object sender, EventArgs e) => {
+				home.pressHomeListItem(name);
+			};
+
+			GestureRecognizers.Add (tap);
+		}
+	}
+}
+
