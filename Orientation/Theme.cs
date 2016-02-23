244035cbbe025e72ab7bec4dfe9f6d7ce671cfa9
draekoon <bsd5129@psu.edu>
2016-02-22 22:31:52 -0500

    Added Theme class in prep for centralized theme controls Fixed up some Home Screen formatting

---
+++ Orientation/Theme.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Orientation
+{
+	public class Theme
+	{
+		private static bool isDark = true;
+
+		private static Color lightThemeBackgroundColor = Color.FromHex("#EEEEEE");
+		private static Color lightThemeTextColor = Color.FromHex("#000000");
+
+		private static Color darkThemeBackgroundColor = Color.FromHex("#303030");
+		private static Color darkThemeTextColor = Color.FromHex("#BBBBBB");
+
+		public static Color getBackgroundColor() {
+			if (isDark)
+				return darkThemeBackgroundColor;
+			else
+				return lightThemeBackgroundColor;
+		}
+
+		public static Color getTextColor() {
+			if (isDark)
+				return darkThemeTextColor;
+			else
+				return lightThemeTextColor;
+		}
+
+		public static void setDarkTheme(bool darkTheme) {
+			isDark = darkTheme;
+		}
+
+		public static bool isDarkTheme() {
+			return isDark;
+		}
+	}
+}
+
