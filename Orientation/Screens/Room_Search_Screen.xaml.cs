7aa693fa19c0cb1c950ef6e562b784cf08aaad21
draekoon <bsd5129@psu.edu>
2016-02-23 01:48:49 -0500

    Added Tab Menu to Room Search Screen Fixed formatting issue with Room Search Screen

--- Orientation/Screens/Room_Search_Screen.xaml.cs
+++ Orientation/Screens/Room_Search_Screen.xaml.cs
@@ -10,6 +10,10 @@ namespace Orientation
 		public Room_Search_Screen ()
 		{
 			InitializeComponent ();
+			bottomLayout.Children.Add (new TabMenu(3));
+
+			roomNumber.WidthRequest = (int)(0.8 * ((Orientation.App)App.Current).getScreenSize().Width);
+			buildingName.WidthRequest = (int)(0.8 * ((Orientation.App)App.Current).getScreenSize().Width);
 		}
 	}
 }
