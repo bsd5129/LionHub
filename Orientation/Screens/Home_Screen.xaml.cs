7a3d784c21e038c4110e3ab87f1e3b1b942fa82b
Brett <brettlashier@hotmail.com>
2016-02-22 21:48:49 -0500

    * Room_Search_Screen.xaml: * Event_Results_Screen.xaml: Wrong class in root tag
    * Home_Screen.xaml.cs: Home Screen now responds to Room Search
    
    * Room_Results_Screen.xaml: Wrong class in Root tag

--- Orientation/Screens/Home_Screen.xaml.cs
+++ Orientation/Screens/Home_Screen.xaml.cs
@@ -66,7 +66,7 @@ namespace Orientation
 		public void pressRoomHomeListItem()
 		{
 			//Uncomment the following when code is set
-			//Navigation.PushAsync (new Room_Search_Screen ());
+			Navigation.PushAsync (new Room_Search_Screen ());
 
 		}
 
