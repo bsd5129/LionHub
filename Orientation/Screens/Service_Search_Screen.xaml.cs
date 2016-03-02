bb45795def476d9031282c2388f5677e2266260e
Brett <brettlashier@hotmail.com>
2016-03-01 23:56:20 -0500

    -Added data collection from SQLiteDatabase to populate the UI -Added the ability to select an item from the list and push it onto the stack and calls the search result class

--- Orientation/Screens/Service_Search_Screen.xaml.cs
+++ Orientation/Screens/Service_Search_Screen.xaml.cs
@@ -1,19 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using SQLite.Net;
 using Xamarin.Forms;
 
 namespace Orientation
 {
 	public partial class Service_Search_Screen : ContentPage
 	{
-		public Service_Search_Screen ()
+		public Service_Search_Screen()
 		{
-			InitializeComponent ();
-			NavigationPage.SetHasBackButton (this, false);
-      bottomLayout.Children.Add(new TabMenu(1));
+			InitializeComponent();
+			NavigationPage.SetHasBackButton(this, false);
+			bottomLayout.Children.Add(new TabMenu(1));
 
-			setTheme ();
+			setTheme();
+
+			searchBar.TextChanged += (object sender, TextChangedEventArgs e) =>
+			{
+				onClick();
+			};
+
+			servicesList.ItemSelected += (Object sender, SelectedItemChangedEventArgs sel) =>
+			{
+				if (sel.SelectedItem != null)
+				{
+					servicesList.SelectedItem = null;
+					this.Navigation.PushAsync(new Service_Results_Screen());
+				}
+			};
 		}
 
 		public void setTheme()
@@ -22,6 +36,18 @@ namespace Orientation
 			servicesList.BackgroundColor = Theme.getBackgroundColor();
 			searchBar.BackgroundColor = Theme.getBackgroundColor();
 		}
+
+		public void onClick()
+		{
+			SQLiteConnection connection = DependencyService.Get<IDatabaseHandler>().getDBConnection();
+			var service = connection.Table<Service>();
+			List<string> names = new List<string>();
+			foreach (var s in service)
+				names.Add(s.name);
+
+			servicesList.ItemsSource = names;
+		}
+
 	}
 }
 
