c39fcab9956dd242e78c6df46a2560592327e600
Brett <brettlashier@hotmail.com>
2016-03-16 11:33:58 -0400

    Function added to convert coordinates to meters.

--- Orientation/Screens/Where_Am_I_Screen.xaml.cs
+++ Orientation/Screens/Where_Am_I_Screen.xaml.cs
@@ -22,6 +22,20 @@ namespace Orientation
 			searchingLabel.TextColor = Theme.getTextColor();
 			indicator.Color = Theme.getTextColor();
 		}
+
+		public float coordToMeters(float startLat, float destLat, float startLong, float destLong)
+		{
+			float R = 6378.137F;
+			float dLat = (destLat - startLat) * (float)(Math.PI / 180);
+			float dLon = (destLong - startLong) * (float)(Math.PI / 180);
+			float A = (float)(Math.Sin(dLat / 2) * Math.Sin(dLat / 2) * Math.Cos(startLat * Math.PI / 180) * Math.Cos(destLat * Math.PI / 180)
+			                  * Math.Sin(dLon / 2) * Math.Sin(dLon/2));
+
+			float C = (float)(2 * Math.Atan2(Math.Sqrt(A), Math.Sqrt(1 - A)));
+			float D = R * C;
+
+			return D * 1000;
+		}
 	}
 }
 
