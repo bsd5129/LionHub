402adfa9112582a6e39c9e5e089dce482ac84999
psuProject <nhn5026@psu.edu>
2016-04-13 15:00:03 -0400

    service search and scavenger hunt screens updated

--- Orientation/Screens/Service_Search_Screen.xaml.cs
+++ Orientation/Screens/Service_Search_Screen.xaml.cs
@@ -30,18 +30,26 @@ namespace Orientation
       return category;
     }
 
+    public Service getServiceData(string name) {
+      SQLiteConnection connection = DependencyService.Get<IDatabaseHandler>().getDBConnection();
+      TableQuery<Service> query = from s in connection.Table<Service>() where s.name.Equals(name) select s;
+      Service service = query.FirstOrDefault();
+      connection.Close();
+      return service;
+    }
+
     public void pressServiceListItem(Object sender, ItemTappedEventArgs args) {
 			if (args.Item != null)
 			{
         if (category != null || (searchBar.Text != null && searchBar.Text.Length > 0))
-          Navigation.PushAsync(new Service_Results_Screen(((ServiceCell)args.Item).service));
+          Navigation.PushAsync(new Service_Results_Screen(getServiceData(args.Item.ToString())));
         else
           Navigation.PushAsync(new Service_Search_Screen(args.Item.ToString()));
 			}
 		}
 
 		public void enterTextIntoSearchBar(Object sender, TextChangedEventArgs e) {
-			queryFilteredListOfServices(searchBar.Text);
+			queryFilteredServices(searchBar.Text);
 		}
 
 		public void setTheme()
@@ -56,12 +64,12 @@ namespace Orientation
 			SQLiteConnection connection = DependencyService.Get<IDatabaseHandler>().getDBConnection();
       var services = connection.Table<Service>().OrderBy(s => s.name);
 
-      List<ServiceCell> names = new List<ServiceCell>();
+			List<string> names = new List<string>();
 			
       foreach (var service in services)
 			{
         if (service.category.Equals(category))
-          names.Add(new ServiceCell(service));
+				  names.Add(service.name);
 			}
 			
       connection.Close();
@@ -72,18 +80,18 @@ namespace Orientation
       SQLiteConnection connection = DependencyService.Get<IDatabaseHandler>().getDBConnection();
       var services = connection.Table<Service>().OrderBy(s => s.category);
 
-      List<ServiceCell> categories = new List<ServiceCell>();
+      List<string> categories = new List<string>();
 
       foreach (var service in services) {
-        if (!categories.Contains(new ServiceCell(service, true)))
-          categories.Add(new ServiceCell(service, true));
+        if (!categories.Contains(service.category))
+          categories.Add(service.category);
       }
 
       connection.Close();
       servicesList.ItemsSource = categories;
     }
 
-		public void queryFilteredListOfServices(String filter)
+		public void queryFilteredServices(String filter)
 		{
       if (filter == null || filter.Length == 0) {
         queryListOfServices();
@@ -93,12 +101,12 @@ namespace Orientation
 			SQLiteConnection connection = DependencyService.Get<IDatabaseHandler>().getDBConnection();
      	var services = connection.Table<Service>().OrderBy(s => s.name);
 
-			List<ServiceCell> names = new List<ServiceCell>();
+			List<string> names = new List<string>();
 			
       foreach (var service in services)
 			{
         if(service.name.ToLower().Contains(filter.ToLower()))
-          names.Add(new ServiceCell(service));
+					names.Add(service.name);
 			}
 
       connection.Close();
