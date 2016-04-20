8cb0fdaa82324c83710ff005767127808e3defc8
draekoon <bsd5129@psu.edu>
2016-04-20 13:50:45 -0400

    Added Icon to Android & Prepared settings for release APK

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -1,18 +1,14 @@
 ﻿using System;
 
 using Android.App;
-using Android.Content;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 using XLabs.Forms;
 
 namespace Orientation.Droid
 {
-	[Activity(Label = "LionHub", Icon = "@drawable/icon", Theme = "@android:style/Theme.Holo.Light", ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait, MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity(Label = "LionHub", Icon = "@drawable/ic_launcher", Theme = "@android:style/Theme.Holo.Light", ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait, MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : XFormsApplicationDroid
 	{
 		protected override void OnCreate(Bundle bundle)
