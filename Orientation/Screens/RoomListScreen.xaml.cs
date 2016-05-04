3244efe2ed205704e45cae5ca19662c06ddcb8a9
draekoon <bsd5129@psu.edu>
2016-05-04 17:34:44 -0400

    Added some files that weren't detected as modified by the IDE

---
+++ Orientation/Screens/RoomListScreen.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using SQLite.Net;
+using Xamarin.Forms;
+
+namespace Orientation {
+  public partial class RoomListScreen : ContentPage {
+    public RoomListScreen(string building) {
+      InitializeComponent();
+
+      queryRooms(building);
+      setTheme();
+    }
+
+    public void setTheme() {
+      BackgroundColor = Theme.getBackgroundColor();
+      roomsList.BackgroundColor = Theme.getBackgroundColor();
+    }
+
+    public void queryRooms(string building) {
+      SQLiteConnection con = DependencyService.Get<IDatabaseHandler>().getDBConnection();
+      var rooms = con.Table<Room>().Where(r => r.buildingName.ToLower().Equals(building.ToLower())).OrderBy(r => r.roomNumber);
+
+      List<RoomCell> roomList = new List<RoomCell>();
+
+      foreach (Room r in rooms) {
+        roomList.Add(new RoomCell(r));
+      }
+
+      roomsList.ItemsSource = roomList;
+
+      con.Close();
+    }
+
+    public void pressRoomListItem(object sender, ItemTappedEventArgs args) {
+      if (args.Item != null) {
+        ((NavigationPage)App.Current.MainPage).PushAsync(new Room_Results_Screen(((RoomCell)args.Item).room));
+      }
+    }
+  }
+}
